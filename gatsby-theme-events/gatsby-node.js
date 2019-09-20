91405bb303dd13cef64c747e31ad9eb9a8adc855
white311 <software.developer112@gmail.com>
2019-09-20 06:47:41 +0300

    create data driven pages

--- gatsby-theme-events/gatsby-node.js
+++ gatsby-theme-events/gatsby-node.js
@@ -46,3 +46,41 @@ exports.createResolvers = ({ createResolvers }) => {
     }
   })
 }
+
+// 4. query for events and create pages
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const basePath = '/';
+  actions.createPage({
+    path: basePath,
+    component: require.resolve('./src/templates/events.js')
+  });
+
+  const result = await graphql(`
+    query {
+      allEvent(sort: {fields: startDate, order: ASC }) {
+        nodes {
+          id
+          slug
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    reporter.panic('error loading events', reporter.errors);
+    return;
+  }
+
+  const events = result.data.allEvent.nodes;
+
+  events.forEach(event => {
+    const slug = event.slug;
+    actions.createPage({
+      path: slug,
+      component: require.resolve('./src/templates/event.js'),
+      context: {
+        eventID: event.id
+      }
+    });
+  });
+};
